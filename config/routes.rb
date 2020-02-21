10a81cfe54fc16e887d1153ba28a83aa1dd29a2c
Manuel Salinas <msalinasdiaz@gmail.com>
2020-02-21 18:45:38 -0300

    crea modelos, index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'stories/index'
+  get 'stories/show'
   devise_for :users
-  root to: "home#index"
-
+  root to: "stories#index"
+resources :user, :stories
 end
